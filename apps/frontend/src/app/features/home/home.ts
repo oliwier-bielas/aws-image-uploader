33fccaa897e52eef2466547a85d0641edab79266
oliwier-bielas <oliwierbielas.praca@gmail.com>
2026-07-11 20:16:15 +0200

    feat: Implement image grid in home view + connect images with API

--- apps/frontend/src/app/features/home/home.ts
+++ apps/frontend/src/app/features/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Button } from '../../components/button/button';
+import { ImageGrid } from '../../components/image-grid/image-grid';
+import type { Image } from '../../core/interfaces/image.interface';
+import { HomeService } from './home.service';
 
 @Component({
   selector: 'aiu-home',
   standalone: true,
-  imports: [Button],
+  imports: [Button, ImageGrid],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  private readonly homeService = inject(HomeService);
+
+  public readonly uploadedImages = toSignal<Image[]>(this.homeService.getUploadedImages());
 
 }
