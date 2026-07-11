c87c763284c42c145c3cc6783bdcce2480a95f73
oliwier-bielas <oliwierbielas.praca@gmail.com>
2026-07-11 16:38:12 +0200

    feat: Add home view and routing

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+    },
+    {
+        path: 'home',
+        loadChildren: () => import('./features/home/home.routes').then(({ homeRoutes }) => homeRoutes)
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+    },
+];
