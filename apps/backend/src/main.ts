e0dad1e467fc7b32964f36be4e0a844a96e9a46d
oliwier-bielas <oliwierbielas.praca@gmail.com>
2026-07-11 16:07:29 +0200

    feat: enable CORS for Angular application

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'http://localhost:4200',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
