95fda343b90f79a1d8d0b5f693071b3865ccf17e
oliwier-bielas <oliwierbielas.praca@gmail.com>
2026-07-11 19:09:46 +0200

    feat: Add basic button component

---
+++ apps/frontend/src/app/components/button/button.models.ts
@@ -0,0 +1,10 @@
+//enums
+export enum ButtonVariantEnum {
+    FILLED = 'filled',
+    TONAL = 'tonal',
+    OUTLINED = 'outlined',
+}
+
+
+// types
+export type ButtonVariantType = `${ButtonVariantEnum}`;
---
+++ apps/frontend/src/app/components/button/button.ts
@@ -0,0 +1,15 @@
+import { Component, input, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { ButtonVariantEnum, ButtonVariantType } from './button.models';
+
+@Component({
+  selector: 'aiu-button',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './button.html',
+  styleUrl: './button.scss',
+})
+export class Button {
+  public label = input.required<string>();
+  public variant = input<ButtonVariantType>(ButtonVariantEnum.FILLED);
+}
